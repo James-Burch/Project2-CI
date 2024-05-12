ceccf185cbf985bcee6bec413503e3c0051841bb
James Burch <burchjames6@gmail.com>
2024-05-12 22:35:32 +0000

    Add JS and CSS to open and close the how to play menu

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Code to open and close the how to play menu
+ */
+document.getElementById('showButton').addEventListener('click', function() {
+    document.getElementById('how-to-play-box').style.display = 'block';
+});
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('how-to-play-box').style.display = 'none';
+});
