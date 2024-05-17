9b0253ce15bff79d4668081b69f13cb2f1754c9e
James Burch <burchjames6@gmail.com>
2024-05-17 20:44:26 +0000

    Change structure of HTML and JS code for the game screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+
+//DOM elements for the game screen
+const prevCar = document.getElementById("prev-car");
+const prevCarName = document.getElementById("prev-car-name");
+const prevCarPower = document.getElementById("prev-car-power");
+const nextCar = document.getElementById("next-car");
+// DOM elements for deciding which game to open
+const mainMenu = document.getElementById("main-menu");
+const gameScreen = document.getElementById("game-screen");
+const gameButton = document.querySelectorAll(".game-btn");
+const gameContent = document.querySelectorAll(".game-content");
+
 // JS Code to open and close the how to play menu
 document.getElementById('showButton').addEventListener('click', function() {
     document.getElementById('how-to-play-box').style.display = 'block';
@@ -6,20 +18,41 @@ document.getElementById('close-btn').addEventListener('click', function() {
     document.getElementById('how-to-play-box').style.display = 'none';
 });
 
-// Display the game that the user clicks on
-const mainMenu = document.getElementById("main-menu");
-const gameScreen = document.getElementById("game-screen");
-const gameButton = document.querySelectorAll(".game-btn");
-const gameContent = document.querySelectorAll(".game-content");
+
+let score = 0;
+let prevCar = getRandomCar();
+let nextCar = getRandomCarExcluding(prevCar);
+// Generate random number
+function getRandomCar() {
+    return Math.floor(Math.random() * searchCars.length);
+ }
+// Ensure the same car is not picked twice in a row
+function getRandomCarExcluding(excludeCar) {
+    let excluding;
+    do {
+        excluding = getRandomCar();
+    } while (excluding === getRandomCar());
+    return excluding;
+} 
+// Displays the cars image and details
+function displayCar() {
+
+}
 
 
-// Add event listeners for the game buttons
-gameButton.forEach(button => {
-    button.addEventListener("click", () => {
-        const gameName = button.getAttribute("data-game");
-        showGame(gameName);
-    });
-}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Function to decide which game has been clicked and what to display
 function showGame(gameName) {
@@ -37,39 +70,17 @@ function showGame(gameName) {
     // Show the content for the selected game
     const selectedGame = document.getElementById(gameName);
     selectedGame.style.display = "block";
-
-    startGame();
 }
+// Add event listeners for the game buttons
+gameButton.forEach(button => {
+    button.addEventListener("click", () => {
+        const gameName = button.getAttribute("data-game");
+        showGame(gameName);
+    });
+}); 
 
-// Generate random number
- function generateRandomNumber() {
-    return Math.floor(Math.random() * searchCars.length);
- }
-//DOM elements for the game screen
-const prevCar = document.getElementById("prev-car");
-const prevCarName = document.getElementById("prev-car-name");
-const prevCarPower = document.getElementById("prev-car-power");
-const nextCar = document.getElementById("next-car");
-// Starts the game
 
-function startGame() {
-    /**
-     * Generates 2 random numbers,
-     * Uses random numbers to pick 2 starter cars
-     */
-    randomNumber1 = generateRandomNumber();
-    randomNumber2 = generateRandomNumber();
-    console.log(randomNumber1);
-    console.log(randomNumber2)
-    /**
-     * Check if both numbers are the same,
-     * If they are then generate another number
-     */
-    while (randomNumber1 === randomNumber2) {
-        randomNumber1 === generateRandomNumber();
-    }
-    //Sets the background image to the first random car
-    prevCar.style.backgroundImage = `url(${searchCars[randomNumber1].image})`;
-    prevCarName.textContent = searchCars[randomNumber1].car;
-    prevCarPower.textContent = searchCars[randomNumber1].carPower;
-}
+
+
+
+
